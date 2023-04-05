bed822d380abd0a22a6307622f34fe0229f77f03
NFARAdmin <admin@autest.net>
2023-04-05 12:06:01 -0700

    updated changes related to ESM

---
+++ test/specs/key_presses.spec.js
@@ -0,0 +1,16 @@
+//OLD and works with NodeJS16
+// const KeyPressesPage = require('../pageobjects/key_press.page');
+
+//NEW and works with NodeJS 18 and above
+import KeyPressesPage from '../pageobjects/key_press.page.js';
+
+describe('My Key Press application', () => {
+    it('should display what is entered into the input', async () => {
+        await KeyPressesPage.open();
+
+        await KeyPressesPage.pressAkey('C');
+
+        await expect(KeyPressesPage.keyPressResult).toHaveTextContaining(
+            'You entered: D');
+    });
+});
